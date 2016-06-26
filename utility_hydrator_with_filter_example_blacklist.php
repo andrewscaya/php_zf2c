c9563937367c225f5281ffcc8603b691957a48bc
Andrew Caya <andrewscaya@yahoo.ca>
2016-06-25 20:54:29 -0400

    2016-06-22 AC: Adds the utility_hydrator_with_filter_example_blacklist.php script and renames the utility_hydrator_with_filter_example_1.php script accordingly.

---
+++ utility_hydrator_with_filter_example_blacklist.php
@@ -0,0 +1,32 @@
+<?php
+
+class Test
+{
+    protected $filtered = 'FILTERED';
+    protected $testOne  = 'TEST1';
+    protected $testTwo  = 'TEST2';
+
+    public function getFiltered() { return $this->filtered; }
+    public function getTestOne()  { return $this->testOne;  }
+    public function getTestTwo()  { return $this->testTwo;  }
+    public function setFiltered($a) { $this->filtered = $a; }
+    public function setTestOne($a)  { $this->testOne  = $a; }
+    public function setTestTwo($a)  { $this->testTwo  = $a; }
+}
+
+include 'init_autoloader.php';
+
+use Zend\Stdlib\Hydrator\ClassMethods;
+use Zend\Stdlib\Hydrator\Filter\FilterComposite;
+
+$hydrator = new ClassMethods(false);
+
+// MethodMatchFilter will blacklist by default
+$hydrator->addFilter(
+    'getfiltered',
+    new Hydrator\Filter\MethodMatchFilter('getFiltered'),
+    FilterComposite::CONDITION_AND
+);
+
+$result = $hydrator->extract(new Test());
+\Zend\Debug\Debug::dump($result);
